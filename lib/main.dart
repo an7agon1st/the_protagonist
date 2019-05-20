0bfc77563018cf4a7bb0415d8ffc59646af9c8b3
Tanzilur Rahman <rahman.tanzilur@outlook.com>
2019-05-20 18:34:58 +0530

    Made responsive, added drawer for landscape view

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter_web/material.dart';
 
 import './widgets/myInkWell.dart';
 import './widgets/myPages.dart';
+import './widgets/myDrawerElements.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,6 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'The Protagonist',
       theme: ThemeData(
+        backgroundColor: Colors.orange[100],
+        //scaffoldBackgroundColor: Colors.blue,
         fontFamily: 'Dollie Demo',
         primaryColor: Color(0xFFFFE0b2),
         textTheme: TextTheme(
@@ -39,6 +42,39 @@ class MyHomePage extends StatelessWidget {
     screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
+      drawer: screenHeight > screenWidth
+          ? Drawer(
+              child: ListView(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(bottom:8.0),
+                    child: AppBar(
+                      elevation: 0.5,
+                      automaticallyImplyLeading: false,
+                      title: Text(
+                        'The Protagonist',
+                        style: TextStyle(
+                          fontSize: 14.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  MyDrawerElement(
+                    title: 'Portfolio',
+                    context: context,
+                  ),
+                  MyDrawerElement(
+                    title: 'Projects',
+                    context: context,
+                  ),
+                  MyDrawerElement(
+                    title: 'Shop',
+                    context: context,
+                  ),
+                ],
+              ),
+            )
+          : null,
       body: Stack(
         children: [
           Image.asset(
@@ -50,31 +86,33 @@ class MyHomePage extends StatelessWidget {
           ),
           Padding(
             padding: EdgeInsets.only(
-                left: screenWidth * 0.06,
-                right: screenWidth * 0.06,
+                left: screenWidth * 0.10,
+                right: screenWidth * 0.10,
                 top: screenHeight * 0.03),
             child: CustomScrollView(
               slivers: <Widget>[
                 SliverAppBar(
-                  actions: <Widget>[
-                    MyInkWell(
-                      context: context,
-                      title: 'Portfolio',
-                    ),
-                    MyInkWell(
-                      context: context,
-                      title: 'Projects',
-                    ),
-                    MyInkWell(
-                      context: context,
-                      title: 'Shop',
-                    ),
-                    IconButton(
-                      iconSize: 30,
-                      padding: EdgeInsets.all(10.0),
-                      icon: Icon(Icons.shopping_cart),
-                    ),
-                  ],
+                  actions: screenWidth > screenHeight
+                      ? <Widget>[
+                          MyInkWell(
+                            context: context,
+                            title: 'Portfolio',
+                          ),
+                          MyInkWell(
+                            context: context,
+                            title: 'Projects',
+                          ),
+                          MyInkWell(
+                            context: context,
+                            title: 'Shop',
+                          ),
+                          // IconButton(
+                          //   iconSize: 30,
+                          //   padding: EdgeInsets.all(10.0),
+                          //   icon: Icon(Icons.shopping_cart),
+                          // ),
+                        ]
+                      : null,
                   floating: false,
                   pinned: true,
                   snap: false,
@@ -99,7 +137,7 @@ class MyHomePage extends StatelessWidget {
                         Expanded(
                           //height: screenHeight,
                           child: Image.asset(
-                            'toolbar.png',
+                            'toolbar.jpeg',
                             //height: screenHeight,
                             width: screenWidth,
                             //colorBlendMode: BlendMode.difference,
