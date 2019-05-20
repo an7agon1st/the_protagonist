fd097a994f5a12feee7aca7711231caf0ee946cb
Tanzilur Rahman <rahman.tanzilur@outlook.com>
2019-05-20 06:13:56 +0530

    UI Polished

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter_web/material.dart';
 
+import './widgets/myInkWell.dart';
+import './widgets/myPages.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -9,9 +12,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'The Protagonist',
       theme: ThemeData(
-        primarySwatch: Colors.orange,
+        fontFamily: 'Dollie Demo',
+        primaryColor: Color(0xFFFFE0b2),
         textTheme: TextTheme(
           title: TextStyle(
+            fontFamily: 'Dollie Demo',
             color: Colors.grey,
           ),
         ),
@@ -34,89 +39,97 @@ class MyHomePage extends StatelessWidget {
     screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(
-            vertical: screenHeight * 0.03, horizontal: screenWidth * 0.06),
-        child: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              actions: <Widget>[
-                InkWell(
-                  child: Container(
-                    padding: EdgeInsets.all(20.0),
-                    width: 100.0,
-                    child: Text('Protfolio'),
+      body: Stack(
+        children: [
+          Image.asset(
+            'background_img.jpg',
+            width: screenWidth,
+            height: screenHeight,
+            alignment: Alignment.center,
+            fit: BoxFit.fill,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+                vertical: screenHeight * 0.03, horizontal: screenWidth * 0.06),
+            child: CustomScrollView(
+              slivers: <Widget>[
+                SliverAppBar(
+                  actions: <Widget>[
+                    MyInkWell(
+                      context: context,
+                      title: 'Portfolio',
+                    ),
+                    MyInkWell(
+                      context: context,
+                      title: 'Projects',
+                    ),
+                    MyInkWell(
+                      context: context,
+                      title: 'Shop',
+                    ),
+                    IconButton(
+                      iconSize: 30,
+                      padding: EdgeInsets.all(10.0),
+                      icon: Icon(Icons.shopping_cart),
+                    ),
+                  ],
+                  floating: false,
+                  pinned: true,
+                  snap: false,
+                  elevation: 0.5,
+                  expandedHeight: screenHeight,
+                  flexibleSpace: FlexibleSpaceBar(
+                    title: Padding(
+                      padding: const EdgeInsets.only(top: 5.0),
+                      child: Text(
+                        'The Protagonist',
+                        style: TextStyle(
+                          //textBaseline: TextBaseline.alphabetic,
+                          fontFamily: 'Dollie Demo',
+                          fontSize: 14,
+                          //color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    background: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      children: <Widget>[
+                        Expanded(
+                          //height: screenHeight,
+                          child: Image.asset(
+                            'toolbar.png',
+                            //height: screenHeight,
+                            width: screenWidth,
+                            //colorBlendMode: BlendMode.difference,
+                            fit: BoxFit.fitHeight,
+                          ),
+                        ),
+                      ],
+                    ),
+                    centerTitle: true,
                   ),
                 ),
-                InkWell(
-                  child: Container(
-                    width: 100.0,
-                    padding: EdgeInsets.all(20.0),
-                    child: Text('Projects'),
+                SliverFillRemaining(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: MyPage(
+                      context,
+                      color: Colors.orange[100],
+                      child: Text('Page 1'),
+                    ),
                   ),
                 ),
-                InkWell(
-                  child: Container(
-                    width: 100.0,
-                    padding: EdgeInsets.all(20.0),
-                    child: Text('Shop'),
+                SliverFillRemaining(
+                  child: MyPage(
+                    context,
+                    color: Colors.red[100],
+                    child: Text('Page 2'),
                   ),
                 ),
               ],
-              floating: false,
-              pinned: true,
-              snap: false,
-              elevation: 0.5,
-              expandedHeight: screenHeight,
-              flexibleSpace: FlexibleSpaceBar(
-                title: Text(
-                  'The Protagonist',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-                background: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    Expanded(
-                      //height: screenHeight,
-                      child: Image.asset(
-                        'toolbar.png',
-                        //height: screenHeight,
-                        width: screenWidth,
-                        //colorBlendMode: BlendMode.difference,
-                        fit: BoxFit.fitHeight,
-                      ),
-                    ),
-                  ],
-                ),
-                centerTitle: true,
-              ),
             ),
-            SliverFillRemaining(
-              child: Container(
-                color: Colors.orange[100],
-                width: screenWidth,
-                height: screenHeight,
-                padding: EdgeInsets.symmetric(
-                    vertical: screenWidth * 0.2,
-                    horizontal: screenHeight * 0.2),
-                child: Text('Page 1'),
-              ),
-            ),
-            SliverFillRemaining(
-              child: Container(
-                color: Colors.red[100],
-                width: screenWidth,
-                height: screenHeight,
-                padding: EdgeInsets.symmetric(
-                    vertical: screenWidth * 0.2,
-                    horizontal: screenHeight * 0.2),
-                child: Text('Page 2'),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
