08d49006f0f0490da04abaf03b870fbb3c2c556d
Tanzilur Rahman <rahman.tanzilur@outlook.com>
2019-05-22 07:12:03 +0530

    Test Deployment Fully Automated

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,16 @@
 rm -rf docs
 cp CNAME ./build/
 mv build docs
+echo deployment ready
+sleep 5
+git add docs
+git commit -m "Test Deploy"
+echo git commited, preparing to push
+sleep 1
+echo .
+sleep 1
+echo .
+sleep 1
+echo pushing
+sleep 1
+git push origin master
